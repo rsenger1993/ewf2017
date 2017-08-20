603bef9b6cd0dd80cc5ffdae5d60d96db5b17cac
Ronny Sabando <rsabando@espol.edu.ec>
2017-08-20 08:26:20 -0500

    crear archivo collector y data base

---
+++ Collector.php
@@ -0,0 +1,26 @@
+<?php
+include_once('dataBase.php');
+
+// Define configuration
+define("DB_HOST", "localhost");
+define("DB_USER", "postgres");
+define("DB_PASS", "postgres");
+define("DB_NAME", "ewfdb");
+
+
+class Collector extends dataBase
+{
+  public static $db;
+  private $host      = DB_HOST;
+  private $username  = DB_USER;
+  private $password  = DB_PASS;
+  private $dbname    = DB_NAME;
+    
+  public function __construct()
+  {
+    self::$db = new dataBase($this->username, $this->password, $this->host, $this->dbname);
+  }
+
+}
+
+?>
