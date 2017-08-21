a35e20c564d19b98972e4d53f70bb710a1881b9c
WashintonApolinario <dekontruccion@gmail.com>
2017-08-20 20:02:06 -0500

    creando archivo Direccion.php

---
+++ Direccion.php
@@ -0,0 +1,39 @@
+<?php
+
+class direccion
+{
+    private $iddireccion;
+    private $nombredireccion;
+    private $descripcion;
+    
+    
+     
+
+      function __construct($iddireccion, $nombredireccion, $descripcion) {
+       $this->iddireccion = $iddireccion;
+       $this->nombredireccion = $nombredireccion;
+       $this->descripcion = $descripcion;      
+     }
+    
+     function setId($iddireccion){
+       $this->iddireccion = $iddireccion;
+     } 
+     function getId(){
+       return $this->iddireccion;
+     } 
+     function setNombreDireccion($nombredireccion){
+       $this->nombredireccion = $nombredireccion;
+     } 
+     function getNombreCompleto(){
+       return $this->nombrecompleto;
+     }
+     function setDescripcion($descripcion){
+       $this->descripcion = $descripcion;
+     } 
+     function getDescripcion(){
+       return $descripcion->descripcion;
+     } 
+     
+}
+
+?> 
