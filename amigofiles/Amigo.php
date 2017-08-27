8b0d486ccbbb02656649da4c24a0959fdc49168f
WashingtonApolinario <dekontruccion@gmail.com>
2017-08-27 17:29:50 -0500

    modoficando Amigo.php

--- amigofiles/Amigo.php
+++ amigofiles/Amigo.php
@@ -1,23 +1,23 @@
 <?php
 
-//include_once 'Usuario.php';
+include_once('../usuariofiles/Usuario.php');
 
-class Amigo //extends Usuario
+class Amigo extends Usuario
 {
     private $idamigo;
+    private $usuario_id;
     
         
-      function __construct($idamigo) {
+      function __construct($idamigo, $usuario_id, $nombreusuario, $clave, $descripcion, $persona_id, $nombrecompleto, $correo, $edad, $telefono, $direccion_id, $direccion_descripcion);
+        parent::__construct($usuario_id, $nombreusuario, $clave, $descripcion, $persona_id, $nombrecompleto, $correo, $edad, $telefono, $direccion_id, $direccion_descripcion );
 
-	//parent::__construct($idusuario, $nombreusuario, $clave, $descripcion);
-
-       $this->idamigo = $idamigo;
-          
+       $this->idamigo = $idamigo;          
      }
     
      function setId($idamigo){
        $this->idamigo = $idamigo;
      } 
+
      function getId(){
        return $this->idamigo;
      } 
