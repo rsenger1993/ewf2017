4305beb4ac21400e6d5daf38a7cfcb8780b41cdd
Ronny Sabando <rsabando@espol.edu.ec>
2017-09-04 05:37:05 -0500

    Validar que no se ingresen datos basura en el registro de usuario

--- usuariofiles/UsuarioCollector.php
+++ usuariofiles/UsuarioCollector.php
@@ -12,21 +12,21 @@ class UsuarioCollector extends Collector
     //echo "linea 1";
     $arrayUsuario= array();        
     foreach ($rows as $c){
-      $aux = new Usuario($c{'id'},$c{'nombreusuario'},$c{'clave'},$c{'descripcion'},$c{'persona_id'});
+      $aux = new Usuario($c{'idusuario'},$c{'nombreusuario'},$c{'clave'},$c{'descripcion'},$c{'persona_id'});
       array_push($arrayUsuario, $aux);
     }
     return $arrayUsuario;        
   }
 
     function showUsuario($idusuario) {
-    $row = self::$db->getRows("SELECT * FROM usuario WHERE id= ?",array("{$idusuario}"));
-    $ObjUsuario= new Usuario($row[0]{'id'}, $row[0]{'nombreusuario'}, $row[0]{'clave'}, $row[0]{'descripcion'},$row[0]{'persona_id'});
+    $row = self::$db->getRows("SELECT * FROM usuario WHERE idusuario= ?",array("{$idusuario}"));
+    $ObjUsuario= new Usuario($row[0]{'idusuario'}, $row[0]{'nombreusuario'}, $row[0]{'clave'}, $row[0]{'descripcion'},$row[0]{'persona_id'});
 
     return $ObjUsuario;        
   }
 
      function showUsuarioByLogin($nombreusuario, $clave) {
-    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.id)
+    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.idpersona)
       INNER JOIN direccion ON (persona.direccion_id = direccion.iddireccion) WHERE nombreusuario= ? AND clave= ?",array("{$nombreusuario}","{$clave}"));
    $arrayUsuario= array();        
     foreach ($row as $c){
@@ -36,9 +36,19 @@ class UsuarioCollector extends Collector
     return $arrayUsuario;      
   }
 
+    function showUsuarioById($idusuario) {
+    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.idpersona)
+      INNER JOIN direccion ON (persona.direccion_id = direccion.iddireccion) WHERE idusuario= ?",array("{$idusuario}"));
+  // $arrayUsuario= array();        
+    foreach ($row as $c){
+      $aux = new Usuario($c{'idusuario'},$c{'nombreusuario'},$c{'clave'},$c{'usuariodescripcion'},$c{'persona_id'},$c{'nombrecompleto'},$c{'correo'},$c{'edad'},$c{'telefono'},$c{'direccion_id'},$c{'direcciondescripcion'},$c{'imgusuario'});
+    }
+    return $aux;        
+  }
+
 
     function countUsuarioByName($nombreusuario) {
-    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.id)
+    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.idpersona)
       INNER JOIN direccion ON (persona.direccion_id = direccion.iddireccion) WHERE nombreusuario= ?",array("{$nombreusuario}"));
    $arrayUsuario= array();        
     foreach ($row as $c){
@@ -49,19 +59,17 @@ class UsuarioCollector extends Collector
   }
 
       function showUsuarioByName($nombreusuario) {
-    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.id)
+    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.idpersona)
       INNER JOIN direccion ON (persona.direccion_id = direccion.iddireccion) WHERE nombreusuario= ?",array("{$nombreusuario}"));
   // $arrayUsuario= array();        
     foreach ($row as $c){
       $aux = new Usuario($c{'idusuario'},$c{'nombreusuario'},$c{'clave'},$c{'usuariodescripcion'},$c{'persona_id'},$c{'nombrecompleto'},$c{'correo'},$c{'edad'},$c{'telefono'},$c{'direccion_id'},$c{'direcciondescripcion'},$c{'imgusuario'});
-      //$per = new persona($c{'id'},$c{'nombrecompleto'},$c{'correo'},$c{'edad'},$c{'telefono'},$c{'direccion_id'});
-     // array_push($arrayUsuario, $aux);
-      //array_push($arrayUsuario, $per);
+
     }
     return $aux;        
   }
        function showUsuarioByImg($idusuario) {
-    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.id)
+    $row = self::$db->getRows("SELECT * FROM usuario INNER JOIN persona ON (usuario.persona_id = persona.idpersona)
       INNER JOIN direccion ON (persona.direccion_id = direccion.iddireccion) WHERE idusuario=?",array("{$idusuario}"));
    //$arrayUsuario= array();
 
@@ -75,12 +83,12 @@ class UsuarioCollector extends Collector
   }
 
       function updateUsuario($idusuario,$nombrecompleto,$correo,$edad,$telefono,$clave,$descripcion) {
-    $insertrow = self::$db->updateRow("UPDATE public.usuario SET nombrecompleto = ? , correo = ? , edad = ? , telefono = ? , clave = ? , descripcion = ?  WHERE id = ?", array("{$nombrecompleto}", "{$correo}", "{$edad}", "{$telefono}", "{$clave}", "{$descripcion}",$idusuario));  
+    $insertrow = self::$db->updateRow("UPDATE public.usuario SET nombrecompleto = ? , correo = ? , edad = ? , telefono = ? , clave = ? , descripcion = ?  WHERE idusuario = ?", array("{$nombrecompleto}", "{$correo}", "{$edad}", "{$telefono}", "{$clave}", "{$descripcion}",$idusuario));  
       
   }
 
       function deleteUsuario($idusuario) {
-    $insertrow = self::$db->deleteRow("DELETE FROM public.usuario WHERE id=?",array("{$idusuario}"));
+    $insertrow = self::$db->deleteRow("DELETE FROM public.usuario WHERE idusuario=?",array("{$idusuario}"));
       
   }
 
--- usuariofiles/crearUsuario.php
+++ usuariofiles/crearUsuario.php
@@ -10,13 +10,6 @@
  $descripcion= $_POST["descripcion"];
  $urlimg= $_FILES;
 
-//GUARDAR IMAGEN EN LA RUTA ESTABLECIDA  "imgperfil/"
-$ruta1= '../imgperfil/'.$nombreusuario.'-'.$urlimg['imagen']['name'];
-move_uploaded_file($urlimg['imagen']['tmp_name'],$ruta1);
-
-//RUTA QUE SE GUARDARA EN LA BASE
-$rutabase= 'imgperfil/'.$nombreusuario.'-'.$urlimg['imagen']['name'];
-
  include_once("../personafiles/PersonaCollector.php");
  include_once("UsuarioCollector.php");
  include_once("../direccionfiles/DireccionCollector.php");
@@ -40,21 +33,50 @@ print(count($arrayCorreo));
 	elseif (count($arrayCorreo)>0) {
 		 echo "ese correo ya existe";
 	}
+	elseif (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+    echo "Esta dirección de correo ($correo) es invalida ";
+	}
+	elseif (!is_numeric($edad)) { 
+	echo "Ingresa solo numeros en el campo edad";
+	}
+	elseif (!is_numeric($telefono)) { 
+	echo "Ingresa un numero de telefono valido";
+	}
+	elseif (strlen($telefono) != 8) { 
+	echo "El numero de telefono debe tener 8 digitos sin contar el 0";
+	}
+	elseif ($urlimg['imagen']['name']=="")
+ 	{
+ 	echo "Porfavor selecciona una imagen para el platillo </br>";
+ 	}
+ 	 
 	else{
+
+		 if((($urlimg["imagen"]["type"] == "image/png") || ($urlimg["imagen"]["type"] == "image/jpg") || ($urlimg["imagen"]["type"] == "image/jpeg")) && ($urlimg["imagen"]["size"] < 200000))
+ 		 {
+ 		 		
+ 		 //SI TODO ESTA CORRECTO PASAMOS A CREAR EL USUARIO
+		
+		//GUARDAR IMAGEN EN LA RUTA ESTABLECIDA  "imgperfil/"
+		 $ruta1= '../imgperfil/'.$nombreusuario.'-'.$urlimg['imagen']['name'];
+		 move_uploaded_file($urlimg['imagen']['tmp_name'],$ruta1);
+
+		//RUTA QUE SE GUARDARA EN LA BASE
+		 $rutabase= 'imgperfil/'.$nombreusuario.'-'.$urlimg['imagen']['name'];
+
 	 	 echo "</br>";
 	 	 $dirId = $DireccionCollectorObj->insertarDireccion($direccion); //INSERTO DIRECCION
 	 	 print_r($dirId["iddireccion"]);
 	 	 $perId = $PersonaCollectorObj->insertarPersona($nombrecompleto,$correo,$edad,$telefono,$dirId["iddireccion"]); //INSERTO PERSONA
-	 	 $UsuarioCollectorObj->insertarUsuario($nombreusuario,$clave,$descripcion,$perId["id"],$rutabase); //INSERTO USUARIO
+	 	 $UsuarioCollectorObj->insertarUsuario($nombreusuario,$clave,$descripcion,$perId["idpersona"],$rutabase); //INSERTO USUARIO
 	 	 echo "Usuario: ".$nombreusuario." creado correctamente</br>";
-	 	 //$ObjPersona = $PersonaCollectorObj->showPersonaByMail($correo);
-	 	 //print_r("mostrando id de persona: ".$ObjPersona->getIdPersona());
-	 	 //echo "ultimo id de persona: ".$perId["id"];
-	 	 //echo "</br>";
-	 	 //echo "ultimo id de drireccion: ".$dirId["id"];
-	 	 //echo "</br>";
-	 	 //print_r($perId);
-	 	 //echo "</br>";
+
+ 		 } 
+ 		 else{
+			echo "Solo se permiten imagenes jpg, png y con un peso menor a 200kb </br>";
+ 		     }
+ 		 
 		}
 ?>
-<a href="../pages/registrarse.php">Volver</a>
+</br>
+<a href="../pages/index.php">Logearse</a>
