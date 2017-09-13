19f9e490969e180edb4934c866902f0f885d7713
Ronny Sabando <rsabando@espol.edu.ec>
2017-09-13 15:47:14 -0500

    validacion w3c

--- usuariofiles/crearUsuario.php
+++ usuariofiles/crearUsuario.php
@@ -9,51 +9,38 @@
  $direccion= $_POST["direccion"];
  $descripcion= $_POST["descripcion"];
  $urlimg= $_FILES;
-
  include_once("../personafiles/PersonaCollector.php");
  include_once("UsuarioCollector.php");
  include_once("../direccionfiles/DireccionCollector.php");
-
  $PersonaCollectorObj = new PersonaCollector();
  $UsuarioCollectorObj = new UsuarioCollector();
  $DireccionCollectorObj = new DireccionCollector();
  $arrayUsuario = $UsuarioCollectorObj->countUsuarioByName($nombreusuario);
  $arrayCorreo = $PersonaCollectorObj->countPersonaByMail($correo);
-  ?>
-
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>EWF | Creacion de Usuario</title>
-<!-- jQuery-->
 <script src="../js/jquery.min.js"></script>
-<!-- Custom Theme files -->
-<!--theme-style-->
 <link href="../css/style-index2.css" rel="stylesheet" type="text/css" media="all" />
 <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-
-<!--//theme-style-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Kappe Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--fonts-->
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900' rel='stylesheet' type='text/css'>
-<!--//fonts-->
 </head>
 <body>
 		<header id="index-header">
 			<input type="checkbox" id ="btn-menu">
 			<label for="btn-menu"> <img src="../images/menu.png" alt="menu"></label>
 			<nav class="menu">
-
-
- <?php
-
+<?php
     if ($nombrecompleto=="" or $nombreusuario=="" or $clave=="" or  $correo=="" or $edad=="" or $telefono=="" or $direccion=="" or $descripcion=="" or $urlimg=="")
 	{
-		?>
+?>
  						<ul>
 					<li> <a >Error</a></li>
 				</ul>
@@ -62,23 +49,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	 <div class="container">
         <div class="single" id="index-login">
                <div class="top-single">
-				<h3>Porfavor llene todos los campos</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<h3>Porfavor llene todos los campos</h3>	
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>			     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
-
     </div>
 </body>
 </html>
@@ -96,22 +73,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>Ese usuario ya existe</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>	     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
-
     </div>
 </body>
 </html>
@@ -128,22 +95,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>Ese correo ya existe</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>				     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
-
     </div>
 </body>
 </html>
@@ -159,23 +116,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	 <div class="container">
         <div class="single" id="index-login">
                <div class="top-single">
-               	<h3> <?php echo "Esta dirección de correo: ".$correo."es invalida";?> </h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+               	<h3> <?php echo "Esta dirección de correo: ".$correo."es invalida";?> </h3>	
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>			     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
-
     </div>
 </body>
 </html>
@@ -192,18 +139,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>Ingresa solo numeros en el campo edad</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>			     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
@@ -224,18 +162,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>Ingresa un numero de telefono valido</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
+				<div id="boton-centrar">
 					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
@@ -256,18 +185,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>El numero de telefono debe tener 8 digitos sin contar el 0</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>			     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
@@ -288,18 +208,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>Porfavor selecciona una imagen para el Usuario</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>			     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
@@ -337,17 +248,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                	 echo "<h3> Usuario: ".$nombreusuario." creado correctamente </h3>";
                	?>
 				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../index.php" class="button">Logearse</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../index.php" class="button">Logearse</a>				     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
@@ -368,18 +271,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="single" id="index-login">
                <div class="top-single">
 				<h3>Solo se permiten imagenes jpg, jpeg, png y con un peso menor a 200kb</h3>
-				<div class="grid-single">	
-				<div id="espacio"></div>
-				<div class="clear">
-					</br>
-					<center> 
-					<a href="../pages/registrarse.php" class="button">Volver</a>
-					</center>
-					</br>				     
+				<div id="boton-centrar">
+					<a href="../pages/registrarse.php" class="button">Volver</a>				     
 				</div>
-				<div id="espacio"></div>
-				</div>
-
 				<div class="clear"> </div>
 			</div>
            </div>
